88c3b2975bccdee473c9912617e7bc4d7b48555c
LucasRios95 <lucashrios95@gmail.com>
2024-05-28 20:52:14 -0300

    criacao do componente sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
-import { Post } from "./Post";
 import { Header } from "./components/Header";
-import "./styles.css";
+import { Post } from "./Post";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from "./App.module.css";
+
+import "./global.css";
 
 export function App() {
   return (
     <div>
       <Header />
 
-      <Post
-        author="Lucas Rios"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum a rerum labore similique, animi et, voluptas velit laudantium doloribus odit veritatis! Est saepe illo praesentium blanditiis obcaecati neque laborum quaerat."
-      />
-      <Post
-        author="João Mag"
-        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste ut commodi praesentium veniam reprehenderit sit quaerat placeat tempore doloribus! Sed quaerat natus officiis modi quo velit necessitatibus laudantium autem est!"
-      />
-    </div>
-  )
-}
+      <div className={styles.wrapper}>
+        <Sidebar />
 
+        <main>
+          <Post
+            author="Lucas Rios"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum a rerum labore similique, animi et, voluptas velit laudantium doloribus odit veritatis! Est saepe illo praesentium blanditiis obcaecati neque laborum quaerat."
+          />
+          <Post
+            author="João Mag"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste ut commodi praesentium veniam reprehenderit sit quaerat placeat tempore doloribus! Sed quaerat natus officiis modi quo velit necessitatibus laudantium autem est!"
+          />
+        </main>
+      </div>
+    </div>
+  );
+}
