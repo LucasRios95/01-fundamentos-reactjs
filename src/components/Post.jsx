70819c90d3854c66c2c311027306e84046781a5c
LucasRios95 <lucashrios95@gmail.com>
2024-06-24 12:44:37 -0300

    Adicionando comentario dinamicamente

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,8 +5,18 @@ import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 
 import styles from "./Post.module.css";
+import { useState } from "react";
+
+
+// estado -> variáveis que eu quero que o componente monitore
 
 export function Post({ author, publishedAt, content }) {
+  const [comments, setComments] = useState([
+    1,
+    2,
+    3
+  ]);
+
   const publishedDateFormatted = format(
     publishedAt,
     "d 'de' LLLL 'às' HH:mm'h'",
@@ -19,6 +29,13 @@ export function Post({ author, publishedAt, content }) {
     locale: ptBR,
     addSuffix: true,
   });
+
+  function handleCreateNewComment() {
+    event.preventDefault();
+    
+    setComments([...comments, comments.length + 1]);
+
+  }
 
   return (
     <article className={styles.post}>
@@ -53,7 +70,7 @@ export function Post({ author, publishedAt, content }) {
         })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
         <textarea placeholder="Deixe um comentário" />
@@ -64,9 +81,9 @@ export function Post({ author, publishedAt, content }) {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map(comment => {
+          return <Comment />
+        })}
       </div>
     </article>
   );
